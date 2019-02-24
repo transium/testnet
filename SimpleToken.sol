d91dc707c225d51d7501b0068511326d10581d16
pelisboule <f.peliss@gmail.com>
2019-02-24 10:20:06 +0100

    Init
    Repo first upload

---
+++ SimpleToken.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.5.2;
+
+import "./token/ERC721/ERC721MetadataMintable.sol";
+
+/**
+ * @title SimpleToken
+ * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
+ * Note they can later distribute these tokens as they wish using `transfer` and other
+ * `ERC20` functions.
+ */
+contract SimpleToken is ERC721MetadataMintable {
+    uint8 public constant DECIMALS = 18;
+    uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(DECIMALS));
+
+    /**
+     * @dev Constructor that gives msg.sender all of existing tokens.
+     */
+    constructor ("Simple token", "SPL") public {
+        _mint(msg.sender, INITIAL_SUPPLY);
+    }
+}
